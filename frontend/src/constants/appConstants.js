d0ab741ac861e95a1d2c0c45269c7ae22d224535
Aditya Narayan Mohanty <adimohanty2001@gmail.com>
2026-07-13 21:00:35 +0530

    Added a simple development switch to toggle btwn local & prod backend

--- frontend/src/constants/appConstants.js
+++ frontend/src/constants/appConstants.js
@@ -1,4 +1,16 @@
-export const API_BASE = 'https://clever-wonder-production-94c5.up.railway.app';
+import Constants from 'expo-constants';
+
+const LOCAL_API =
+  'http://10.76.43.47:3001';
+
+const PROD_API =
+  'https://clever-wonder-production-94c5.up.railway.app';
+
+export const API_BASE =
+  Constants.expoConfig?.extra?.useLocalBackend
+    ? LOCAL_API
+    : PROD_API;
+    
 export const XP_STORAGE_KEY = '@focusfoe/xp';
 export const STATS_STORAGE_KEY = '@focusfoe/stats';
 export const ACHIEVEMENTS_STORAGE_KEY = '@focusfoe/achievements';
